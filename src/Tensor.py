3b086cbf9b81d5435c3f2f0d474ae626db760b95
gamedoescool <hi_ishan@outlook.com>
2026-07-02 22:27:50 -0700

    ok maybe this time itll work

--- src/Tensor.py
+++ src/Tensor.py
@@ -1,31 +1,47 @@
 import numpy as np
 
 class TensorNode:
-    def __init__(self,data: np.ndarray, is_param = True, update_policy = None):
+    def __init__(self,data: np.ndarray, is_param = True, dependents = None, forward_update = None, update_policy = None):
         self.data = data
         self.update_policy = update_policy
+        self.forward_update = forward_update
+        self.dependents = dependents
+
         if(self.update_policy == None):
             def dummy_update(lr,gradient):
                 return
             self.update_policy = dummy_update
-        
+        if(forward_update == None):
+            def dummy_update2():
+                return
+            self.forward_update = dummy_update2
+        if(self.dependents == None):
+            self.dependents = []
         self.gradient = np.zeros_like(data)
         self.is_param = is_param
+        self.compiled = False
+        self.topo_sort = []
     
-    def backprop(self,lr:float, outerGrad: np.ndarray):
+    def backprop(self, outerGrad: np.ndarray):
         if(outerGrad.shape != self.data.shape):
             SyntaxError("Gradient must be same shape as the data. This should not happen unless something went very wrong on my side.")
+        self.update_policy(outerGrad)
+    
+    def update_params(self, lr):
         if(self.is_param):
-            self.data -= lr*outerGrad
-        self.update_policy(lr, outerGrad)
+            self.data -= lr*self.gradient
+        self.gradient -= self.gradient
+
     
     def __add__(self, other):
         if(isinstance(other,TensorNode) == False):
             NotImplementedError("cannot add with nontensor")
-        def update_policy(lr:float, gradient: np.ndarray):
-            self.backprop(lr,gradient)
-            other.backprop(lr,gradient)
-        return TensorNode(self.data + other.data, False, update_policy)
+        def update_policy(gradient: np.ndarray):
+            self.gradient += gradient
+            other.gradient += gradient
+        def forward_update():
+            return self.data + other.data
+        return TensorNode(forward_update(), False, forward_update, update_policy)
     
     def __sub__(self, other):
         return self + (-other)
@@ -33,22 +49,29 @@ class TensorNode:
     def __rmul__(self,other):
         if(isinstance(other,float) == False):
             NotImplementedError("cannot multiply with " + other)
-        def update_policy(lr:float, gradient: np.ndarray):
-            self.backprop(lr,other*gradient)
-        return TensorNode(self.data, False, update_policy)
+        def update_policy(gradient: np.ndarray):
+            self.gradient += other*gradient
+        def forward_update():
+            return other*self.data
+        return TensorNode(forward_update(), False, forward_update, update_policy)
     
     def __matmul__(self, other):
         if(isinstance(other,TensorNode) == False):
             NotImplementedError("cannot multiply with nontensor")
         def update_policy(lr:float, gradient: np.ndarray):
             #gradient * self @ other = gradient @ other.T * self = self.T @ gradient * other
-            self.backprop(lr, gradient @ other.data.T)
-            other.backprop(lr, self.data.T @ gradient)
-        return TensorNode(self.data @ other.data, False, update_policy)
+            self.gradient += gradient @ other.data.T
+            other.gradient += self.data.T @ gradient
+        def forward_update():
+            return self.data @ other.data
+        return TensorNode(forward_update(), False, forward_update, update_policy)
+    
+
 
     #TODO:
     #add activation function
-    #i guess go with batch based method because this is a uphill battle
+    #add compile(), compute(np.ndarray input), train(), update() methods.
+    
     
 #interesting engineering thought: the way ive implemented it here makes it basically do depth first topologically backwards. this allows for the same node to perform backprop multiple times. 
 #I think this allows for better model stability as the model takes more microsteps. I'll keep this and see how it performs.
