53e847a9636c9f0234c6131b9a7387b1ae993c57
gamedoescool <hi_ishan@outlook.com>
2026-07-03 12:58:57 -0700

    forward_update not really used yet...

--- src/Tensor.py
+++ src/Tensor.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import deque
 
 class TensorNode:
     def __init__(self,data: np.ndarray, is_param = True, dependents = None, forward_update = None, update_policy = None):
@@ -8,7 +9,7 @@ class TensorNode:
         self.dependents = dependents
 
         if(self.update_policy == None):
-            def dummy_update(lr,gradient):
+            def dummy_update(gradient):
                 return
             self.update_policy = dummy_update
         if(forward_update == None):
@@ -18,6 +19,7 @@ class TensorNode:
         if(self.dependents == None):
             self.dependents = []
         self.gradient = np.zeros_like(data)
+        self.temp_grad = np.zeros_like(data)
         self.is_param = is_param
         self.compiled = False
         self.topo_sort = []
@@ -37,11 +39,14 @@ class TensorNode:
         if(isinstance(other,TensorNode) == False):
             NotImplementedError("cannot add with nontensor")
         def update_policy(gradient: np.ndarray):
-            self.gradient += gradient
-            other.gradient += gradient
+            self.temp_grad = gradient
+            self.gradient += self.temp_grad
+
+            other.temp_grad = gradient
+            other.gradient += other.temp_grad
         def forward_update():
             return self.data + other.data
-        return TensorNode(forward_update(), False, forward_update, update_policy)
+        return TensorNode(forward_update(), False, [self,other], forward_update, update_policy)
     
     def __sub__(self, other):
         return self + (-other)
@@ -50,28 +55,82 @@ class TensorNode:
         if(isinstance(other,float) == False):
             NotImplementedError("cannot multiply with " + other)
         def update_policy(gradient: np.ndarray):
-            self.gradient += other*gradient
+            self.temp_grad = other * gradient
+            self.gradient += other*self.temp_grad
         def forward_update():
             return other*self.data
-        return TensorNode(forward_update(), False, forward_update, update_policy)
+        return TensorNode(forward_update(), False, [self], forward_update, update_policy)
     
     def __matmul__(self, other):
         if(isinstance(other,TensorNode) == False):
             NotImplementedError("cannot multiply with nontensor")
-        def update_policy(lr:float, gradient: np.ndarray):
+        def update_policy(gradient: np.ndarray):
             #gradient * self @ other = gradient @ other.T * self = self.T @ gradient * other
-            self.gradient += gradient @ other.data.T
-            other.gradient += self.data.T @ gradient
+            self.temp_grad = gradient @ other.data.T
+            self.gradient += self.temp_grad
+
+            other.temp_grad = self.data.T @ gradient
+            other.gradient += other.temp_grad
         def forward_update():
             return self.data @ other.data
-        return TensorNode(forward_update(), False, forward_update, update_policy)
+        return TensorNode(forward_update(), False, [self,other], forward_update, update_policy)
     
+    def dot(self,other):
+        def update_policy(gradient: np.ndarray):
+            self.temp_grad = gradient*other.data
+            self.gradient += self.temp_grad
+            other.temp_grad = gradient*self.data
+            other.gradient += other.temp_grad
+        def forward_update():
+            return np.sum(self.data * other.data)
+        return TensorNode(forward_update(), False, [self, other], forward_update, update_policy)
+    def compile(self):
+        self.iterator = deque()
+        if(self.data.size != 1):
+            TypeError("Tensor MUST be a scalar in order to compile the pipeline")
+        self.iterator.append([self])
 
+        while(len(self.iterator) != 0):
+            current = self.iterator.popleft()
+            if(len(current) != 0):
+                self.topo_sort.append(current)
+                new_depend = []
+                for node in current:
+                    new_depend += (node.dependents)
+                self.iterator.append(new_depend)
+
+        #gradient prepping
+        self.temp_grad = np.ones_like(self.data)
+
+        #debug return
+        self.compiled = True
+        return self.topo_sort
+    
+    def train(self):
+        if(self.data.size != 1):
+            TypeError("Tensor MUST be a scalar in order to train the pipeline")
+        if (self.compiled == False):
+            RuntimeError("Pipeline must be compiled in order to train")
+        for level in self.topo_sort:
+            #parallel magic maybe?
+            for node in level:
+                node.update_policy(node.temp_grad)
+
+    def update(self, lr: float):
+        if(self.data.size != 1):
+            TypeError("Tensor MUST be a scalar in order to update the pipeline")
+        if (self.compiled == False):
+            RuntimeError("Pipeline must be compiled in order to update")
+        for level in self.topo_sort:
+            #parallel magic maybe?
+            for node in level:
+                node.data -= lr*node.gradient
+                node.gradient -= node.gradient
 
     #TODO:
     #add activation function
     #add compile(), compute(np.ndarray input), train(), update() methods.
-    
+
     
 #interesting engineering thought: the way ive implemented it here makes it basically do depth first topologically backwards. this allows for the same node to perform backprop multiple times. 
 #I think this allows for better model stability as the model takes more microsteps. I'll keep this and see how it performs.
--- src/test_test.py
+++ src/test_test.py
@@ -1,18 +1,33 @@
 import Tensor as t
 import numpy as np
-x = np.random.uniform(0,1,size=(67,1))
-y = np.random.uniform(0,1,size=(3,67))
 
-x = t.TensorNode(x,True)
-y = t.TensorNode(y,True)
 
-z = (y @ x).act()
-#lets try to minimize z^2 (z dot z) guys!
-print(z.data.T @ z.data)
-for i in range(10):
-    z.backprop(0.007/(i*i+2*i+1),2*z.data)
-    z = (y @ x).act()
-    print(z.data.T @ z.data)
+def pipeline(x_1,y_1,train):
+    x = t.TensorNode(x_1,True)
+    y = t.TensorNode(y_1,True)
+
+    z = (y @ x)
+    #lets try to minimize z^2 (z dot z) guys!
+    loss = z.dot(z)
+
+
+    print(loss.data)
+    if(train):
+        k = loss.compile()
+        loss.train()
+        loss.update(0.003)
+    
+
+
+x_1 = np.random.uniform(0,1,size=(10,1))
+y_1 = np.random.uniform(0,1,size=(3,10))
+
+print(x_1)
+print(y_1)
+pipeline(x_1,y_1,True)
+print(x_1)
+print(y_1)
+pipeline(x_1,y_1,False)
 
 
 # print(z.data.T @ z.data)
